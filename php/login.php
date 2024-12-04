bcd70d4ec6611113de8e4b77c64a26701942c649
Dovahkiin-web <ferreira.aaron.aller.ulloa@gmail.com>
2024-12-04 13:05:49 +0100

    Add files via upload

--- php/login.php
+++ php/login.php
@@ -1,1 +1,11 @@
-<?php ?>
+<?php 
+
+if ( isset ( $_REQUEST['btn_login'] ) || isset ( $_REQUEST['btn_register'] ) ){
+    if ( isset ( $_REQUEST['btn_login'] ) ) {
+        echo "Te has logeado";
+    } elseif (isset($_REQUEST['btn_register'])) {
+        echo "Te has registrado";
+    }
+}
+
+?>
